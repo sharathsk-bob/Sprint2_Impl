16a14966e29e923f71db8f43cb937bb1d79cae34
Swetha <justitsit@gmail.com>
2022-11-10 21:27:34 +0530

    swetha

--- src/Components/NutritionModule.js
+++ src/Components/NutritionModule.js
@@ -7,72 +7,38 @@ import './nutrition.css';
 function NutritionModule() {
     //const [input,setInput]=useState("");
   const [weatherData,setWeatherData]=useState([]);
- 
-
   const history=useNavigate();
-
   useEffect(()=>{
     axios.get('http://localhost:8020/nutritionplan/',{headers:{"Content-Type" : "application/json"}})
     .then((data)=>{
-     
-        setWeatherData(data.data);
-       
-      
+        setWeatherData(data.data);  
     }).catch((error)=>setWeatherData({ errorMessage: error.error}));
   },[]);
   
-  
-
- 
-    const getAdmin=()=>{
-        var errorMessage;
-        axios.get('http://localhost:8020/nutritionplan/',{headers:{"Content-Type" : "application/json"}})
-        .then((data)=>
-            console.log(data.data)).catch((error)=>setWeatherData({ errorMessage: error.error}));
-    }
     const Pay=()=>{
         history("/payment")
     }
   return (
-    
     <div >
-        <h3>Nutrition Modules</h3>
+        <h1 className='nutri-head'>Nutrition Modules</h1>
         <form className='container'>
-        {weatherData[0] &&(
+        {weatherData &&(
           <React.Fragment>
-            <div >
-            <p>{weatherData[0].planDiscription}</p>
-            <p>{weatherData[0].created_At}</p>
-            <p>{weatherData[0].updated_At}</p>
-            <p>{weatherData[0].price}</p>
-            <button onClick={Pay}>BUY</button>
-         
+            {weatherData.map((weather)=>(
+              <div key={weather.id} className='nutri-div' >
+            <p className='nutri-p'>{weather.name}</p>
+            <p className='nutri-p'>{weather.planDiscription}</p>
+            <p className='nutri-p'>{weather.created_At}</p>
+            <p className='nutri-p'>{weather.updated_At}</p>
+            <p className='nutri-p'>{weather.price}</p>
+            <button className='nutri-button' onClick={Pay}>BUY</button>
             </div>
-            <div >
-            <p>{weatherData[1].planDiscription}</p>
-            <p>{weatherData[1].created_At}</p>
-            <p>{weatherData[1].updated_At}</p>
-            <p>{weatherData[1].price}</p>
-            <button onClick={Pay}>BUY</button>
-           
-            </div>
-            <div >
-            <p>{weatherData[2].planDiscription}</p>
-            <p>{weatherData[2].created_At}</p>
-            <p>{weatherData[2].updated_At}</p>
-            <p>{weatherData[2].price}</p>
-            <button onClick={Pay}>BUY</button>
-           
-            </div>
+              
+            ))}
             </React.Fragment >)}
-            
-     
      </form>
      {/* <button onClick={getAdmin}>BUY</button> */}
     </div>
-    
-
   )
 }
-
 export default NutritionModule;
